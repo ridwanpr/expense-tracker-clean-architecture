cb74b28a98a2a217c517c4801162129b6ac51dfa
Ridwan <ridwanpr@outlook.com>
2026-01-14 11:24:25 +0700

    fix stringToNumber utility

--- src/shared/utils/stringToNumber.ts
+++ src/shared/utils/stringToNumber.ts
@@ -5,9 +5,6 @@ export const stringToNumber = (
   statusCode: number,
   errorMessage: string
 ): number => {
-  if (!value || value.trim() === "") {
-    throw new ResponseError(statusCode, errorMessage);
-  }
   const result = Number(value);
   if (isNaN(result)) {
     throw new ResponseError(statusCode, errorMessage);
