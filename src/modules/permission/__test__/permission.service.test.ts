391e810c900c0c687458f42a53f20edf25adb5d3
Ridwan <ridwanpr@outlook.com>
2026-01-15 10:34:45 +0700

    add permission unit test

---
+++ src/modules/permission/__test__/permission.service.test.ts
@@ -0,0 +1,56 @@
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import { PermissionRepository } from "../permission.repository.port";
+import { PermissionService } from "../permission.service";
+
+const permissionRepo = {
+  getAllPermissions: vi.fn(),
+  checkPermissionExistBySlug: vi.fn(),
+} as unknown as PermissionRepository;
+
+const permissionService = new PermissionService(permissionRepo);
+
+describe("Permission Service Test", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("should get all permissions", async () => {
+    const mockPermission = [
+      {
+        id: 1,
+        slug: "expense:create",
+        description: "Can create a new expense",
+      },
+      {
+        id: 2,
+        slug: "expense:read",
+        description: "Can view expenses",
+      },
+    ];
+
+    vi.mocked(permissionRepo.getAllPermissions).mockResolvedValue(mockPermission);
+    const result = await permissionService.getAllPermissions();
+
+    expect(result).toEqual(mockPermission);
+  });
+
+  it("should check if permission exist", async () => {
+    const slug = "workspace:create";
+
+    vi.mocked(permissionRepo.checkPermissionExistBySlug).mockResolvedValue(true);
+    const result = await permissionService.checkIfPermissionExist(slug);
+
+    expect(permissionRepo.checkPermissionExistBySlug).toHaveBeenCalledWith(slug);
+    expect(result).toEqual(true);
+  });
+
+  it("should reject permission not found", async () => {
+    const slug = "not found";
+
+    vi.mocked(permissionRepo.checkPermissionExistBySlug).mockResolvedValue(false);
+    const result = await permissionService.checkIfPermissionExist(slug);
+
+    expect(permissionRepo.checkPermissionExistBySlug).toHaveBeenCalledWith(slug);
+    expect(result).toEqual(false);
+  });
+});
